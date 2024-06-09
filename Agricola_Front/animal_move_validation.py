321d567642cec83fcac074b366408c5d907fec97
HiImSUN <kshk08@hanyang.ac.kr>
2024-06-10 02:32:10 +0900

    pass까지 구현 중

---
+++ Agricola_Front/animal_move_validation.py
@@ -0,0 +1,17 @@
+def animal_move_validation(animal_type, positon):
+    log = []
+
+    def check_already_placed():
+        
+
+
+
+
+
+
+
+
+
+
+
+    return ( check_already_placed() and check_same_type() )
